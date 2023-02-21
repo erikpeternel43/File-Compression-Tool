f6b81077ba16591ace0f57c5d42567f7b7c6e9f6
erikpeterne43 <erik.peternel43@gmail.com>
2023-02-21 23:54:13 +0100

    err_checking

--- lib/gui.c
+++ lib/gui.c
@@ -32,34 +32,38 @@ void compress_click()
     int return_status;
     /* Path to file and directory */
     GFile *input_file = gtk_file_chooser_get_file(input);
-    char  *input_file_path = g_file_get_path(input_file);
+    char  *input_path = g_file_get_path(input_file);
     GFile *parent_dir = g_file_get_parent(input_file);
-    char  *parent_dir_path = g_file_get_path(parent_dir);
+    char  *parent_path = g_file_get_path(parent_dir);
     /* Output_file */
-    const gchar *output_file_name = gtk_entry_get_text(GTK_ENTRY(output));
+    const gchar *output_name = gtk_entry_get_text(GTK_ENTRY(output));
     
-    if(input_file && strlen(output_file_name) > 0)
+    if(input_file && strlen(output_name) > 0)
     {
         gtk_label_set_text(progress_label, "Compressing...");
         
         /* Make filepath */
-        size_t len_parent = strlen(parent_dir_path);
-        size_t len_output = strlen(output_file_name);
-        char * output_file_path = calloc(sizeof(char), len_parent + len_output + 7);
+        size_t len_parent = strlen(parent_path);
+        size_t len_output = strlen(output_name);
+        char * output_path = calloc(sizeof(char), len_parent + len_output + 7);
+        if(output_path == NULL)
+            err_sys("Allocating memory for output_path.");
 
-        if(huff_selected()){
-            memcpy(output_file_path, parent_dir_path, len_parent);
+        if(huff_selected())
+        {
+            memcpy(output_path, parent_path, len_parent);
             /* Add slash to path */
-            output_file_path[len_parent] = '/';
-            memcpy(output_file_path + len_parent + 1, output_file_name, len_output);
-            memcpy(output_file_path + len_parent + len_output + 1, ".huff", 5);
+            output_path[len_parent] = '/';
+            memcpy(output_path + len_parent + 1, output_name, len_output);
+            memcpy(output_path + len_parent + len_output + 1, ".huff", 5);
         }
-        else{
-            memcpy(output_file_path, parent_dir_path, len_parent);
+        else
+        {
+            memcpy(output_path, parent_path, len_parent);
             /* Add slash to path */
-            output_file_path[len_parent] = '/';
-            memcpy(output_file_path + len_parent + 1, output_file_name, len_output);
-            memcpy(output_file_path + len_parent + len_output + 1, ".lzw", 4);
+            output_path[len_parent] = '/';
+            memcpy(output_path + len_parent + 1, output_name, len_output);
+            memcpy(output_path + len_parent + len_output + 1, ".lzw", 4);
         }
 
         /* Creating child which will execute program for compress - either huffman or lzw */
@@ -79,19 +83,19 @@ void compress_click()
             if(huff_selected())
             {
                 /* Huffman */
-                return_status = execl("huffman_compression/bin/run", "run", "-c", "-i", input_file_path, "-o", output_file_path, NULL);
+                return_status = execl("huffman_compression/bin/run", "run", "-c", "-i", input_path, "-o", output_path, NULL);
                 if(return_status == -1)
                     err_sys("Process error");
             }
             else
             {
                 /* LZW */
-                return_status = execl("lzw_compression/bin/run", "run", "-c", "-i", input_file_path, "-o", output_file_path, NULL);
+                return_status = execl("lzw_compression/bin/run", "run", "-c", "-i", input_path, "-o", output_path, NULL);
                 if(return_status == -1)
                     err_sys("Process error");
             }
         }
-        free(output_file_path);
+        free(output_path);
     }
     else
     {
@@ -105,32 +109,34 @@ void decompress_click()
     int return_status;
     /* Path to file and directory */
     GFile *input_file = gtk_file_chooser_get_file(input);
-    char  *input_file_path = g_file_get_path(input_file);
+    char  *input_path = g_file_get_path(input_file);
     GFile *parent_dir = g_file_get_parent(input_file);
-    char  *parent_dir_path = g_file_get_path(parent_dir);
+    char  *parent_path = g_file_get_path(parent_dir);
     /* Output_file */
-    const gchar *output_file_name = gtk_entry_get_text(GTK_ENTRY(output));
+    const gchar *output_name = gtk_entry_get_text(GTK_ENTRY(output));
     
-    if(input_file && strlen(output_file_name) > 0)
+    if(input_file && strlen(output_name) > 0)
     {
         gtk_label_set_text(progress_label, "Decompressing...");
         
         /* Make filepath */
-        size_t len_parent = strlen(parent_dir_path);
-        size_t len_output = strlen(output_file_name);
-        char * output_file_path = calloc(sizeof(char), len_parent + len_output + 1);
+        size_t len_parent = strlen(parent_path);
+        size_t len_output = strlen(output_name);
+        char * output_path = calloc(sizeof(char), len_parent + len_output + 1);
+        if(output_path == NULL)
+            err_sys("Allocating memory for output_path.");
 
         if(huff_selected()){
-            memcpy(output_file_path, parent_dir_path, len_parent);
+            memcpy(output_path, parent_path, len_parent);
             /* Add slash to path */
-            output_file_path[len_parent] = '/';
-            memcpy(output_file_path + len_parent + 1, output_file_name, len_output);
+            output_path[len_parent] = '/';
+            memcpy(output_path + len_parent + 1, output_name, len_output);
         }
         else{
-            memcpy(output_file_path, parent_dir_path, len_parent);
+            memcpy(output_path, parent_path, len_parent);
             /* Add slash to path */
-            output_file_path[len_parent] = '/';
-            memcpy(output_file_path + len_parent + 1, output_file_name, len_output);
+            output_path[len_parent] = '/';
+            memcpy(output_path + len_parent + 1, output_name, len_output);
         }
 
         /* Creating child which will execute program for compress - either huffman or lzw */
@@ -150,19 +156,19 @@ void decompress_click()
             if(huff_selected())
             {
                 /* Huffman */
-                return_status = execl("huffman_compression/bin/run", "run", "-d", "-i", input_file_path, "-o", output_file_path, NULL);
+                return_status = execl("huffman_compression/bin/run", "run", "-d", "-i", input_path, "-o", output_path, NULL);
                 if(return_status == -1)
                     err_sys("Process error");
             }
             else
             {
                 /* LZW */
-                return_status = execl("lzw_compression/bin/run", "run", "-d", "-i", input_file_path, "-o", output_file_path, NULL);
+                return_status = execl("lzw_compression/bin/run", "run", "-d", "-i", input_path, "-o", output_path, NULL);
                 if(return_status == -1)
                     err_sys("Process error");
             }
         }
-        free(output_file_path);
+        free(output_path);
     }
     else
     {
